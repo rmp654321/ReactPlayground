d65ee38e924b84506b33e127db00eb75df80fa8b
Patel Rahul <Patel.Rahul@idx.inc>
2024-10-11 22:55:58 +0530

    added reactElement function's child arg and clear console code on page load

--- class-1.js
+++ class-1.js
@@ -4,8 +4,9 @@
 */
 
 document.addEventListener("DOMContentLoaded", function () {
-  //createElement(); //creating elem using vanilla js 
-  reactElement();  //creating elem using react
+  console.clear(); // clean the console
+  //createElement(); //creating elem using vanilla js
+  reactElement(); //creating elem using react
 });
 
 function userInput() {
@@ -21,25 +22,40 @@ function createElement() {
     document.body.appendChild(elem);
     console.log("User's Input is: ", elem.textContent);
   } else {
-    msg.textContent ="You haven't entered anytging. Reload the page & try again.";
+    msg.textContent =
+      "You haven't entered anytging. Reload the page & try again.";
     document.body.appendChild(msg);
     console.log(msg.textContent);
   }
 }
 
-function reactElement(){
-    const userIP = userInput();
-    const elem = React.createElement("ho", {id: "heading", style: {color:"red"}}, userIP); //heading element can be custom aswell like hr/ui/9h.. anything
-    if(userIP != ''){
-      const root2 = ReactDOM.createRoot(document.getElementById("root1")); //root element can have any custom id name
-      root2.render(elem); 
-      console.log("User Input:", userIP) //react created VIRTUAL DOM so it doesnt support function like textContent like js 
-    }
-    else{
-      const errorMsg = "You haven't typed anything , reload the page and try again."
-      const errorElem = React.createElement("span", {class: "error-msg"}, errorMsg);
-      const root2 = ReactDOM.createRoot(document.getElementById("root1"));
-      root2.render(errorElem);
-      console.log(errorMsg);
-    }
+function reactElement() {
+  const userIP = userInput();
+  const elem = React.createElement(
+    "ho",
+    { id: "heading", style: { color: "red" } },
+    userIP
+  ); //heading element can be custom aswell like hr/ui/9h.. anything
+  if (userIP != "") {
+    const root2 = ReactDOM.createRoot(document.getElementById("root1")); //root element can have any custom id name
+    root2.render(elem);
+    console.log("User Input:", userIP); //react created VIRTUAL DOM so it doesnt support function like textContent like js
+  } else {
+    const errorMsg =
+      "You haven't typed anything , reload the page and try again.";
+    const errorElem = React.createElement(
+      "span",
+      { class: "error-msg" },
+      errorMsg
+    );
+    const errorContainer = React.createElement(
+      "div",
+      { class: "error-msg__holder" },
+      [errorElem]
+    ); // [] suggests that errorElem is child of errorContainer
+
+    const root2 = ReactDOM.createRoot(document.getElementById("root1"));
+    root2.render(errorContainer);
+    console.log(errorMsg);
+  }
 }
